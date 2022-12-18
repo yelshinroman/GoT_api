ba69513df3f74bf95140b97338be571567715f54
POMAH-KOPOJlb <POMAH-KOPOJlb>
2022-12-18 19:49:16 +0300

    GoT API Character

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './spiner.css';
+
+const Spinner = () => {
+    return (
+
+        <div class="loadingio-spinner-spin-15gtdu78p3h">
+            <div class="ldio-ge22zpji4i7">
+                <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
+
