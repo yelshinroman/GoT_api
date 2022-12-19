1ed18246d81b407a0143ca073f3430f190343f73
POMAH-KOPOJlb <POMAH-KOPOJlb>
2022-12-19 19:27:01 +0300

    add toggle Random Char

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
-import CharDetails from '../charDetails';
+import ErrorMessage from '../errorMessage';
+import PersonDetails from '../personDetails';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
-};
+import './app.css';
 
-export default App;
+
+export default class App extends Component {
+    state = {
+        showRandomChar: true,
+        error: false
+    }
+    toggleRandomChar = () => {
+        this.setState((state) => {
+            return {
+                showRandomChar: !state.showRandomChar
+            }
+        });
+    }
+    render() {
+        if (this.state.error) {
+            return <ErrorMessage/>
+        }
+        const char = this.state.showRandomChar ? <RandomChar/> : null;
+        return (
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            {char}
+                            <button 
+                                className="toggle-btn"
+                                onClick={this.toggleRandomChar}>Toggle random character</button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <PersonDetails/>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
+}
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -34,7 +34,6 @@ export default class RandomChar extends Component {
 
     updateChar() {
         const id = Math.floor(Math.random()*140+25);
-        // const id = 13333333;
 
         this.gotService.getCharacter(id)
             .then(this.onCharLoaded)
